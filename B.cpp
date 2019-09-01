b028db414b808e3aed8733aef6426ea232adc36c
shivam kumar singh <47945055+shivam-51@users.noreply.github.com>
2019-09-02 00:59:39 +0530

    Update B.cpp

--- B.cpp
+++ B.cpp
@@ -2,15 +2,13 @@
     AUTHOR:shivam51
     IIESTS
 */
+
   #include<bits/stdc++.h>
   using namespace std;
   //
-  #define add	 	accumulate
   #define ll 	 	long long int
   #define rep(i,k,n)    for(int i=k;i<n;i++)
-  #define hs            ios_base::sync_with_stdio(false);cin.tie(NULL);
-  template <class C> void setmin(C &a,C b){if(a>b)a=b;}
-  template <class C> void setmax(C &a,C b){if(a<b)a=b;}
+
   const ll mod=1e9+7;
   const ll MAX=1e5+10;
   //
