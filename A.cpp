9740331c66e4d3b83af8ac08ccc124585ef7def2
shivam kumar singh <47945055+shivam-51@users.noreply.github.com>
2019-09-02 00:55:18 +0530

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -5,23 +5,10 @@
   #include<bits/stdc++.h>
   using namespace std;
   //
-  #define add	 	accumulate
   #define ll 	 	long long int
-  #define ff	 	first
-  #define ss 	 	second
-  #define pb 	 	push_back
-  #define pob		pop_back
-  #define pf 	 	push_front
-  #define pof		pop_front
-  #define mp 	 	make_pair
   #define vll 	 	vector<ll>
-  #define lb 	 	lower_bound
-  #define ub     		upper_bound
-  #define pll 	 	pair<ll,ll>
-  #define vpll   		vector<pll>
-  #define all(v) 		v.begin(), v.end()
+  #define all(v) 	v.begin(), v.end()
   #define rep(i,k,n)    for(int i=k;i<n;i++)
-  #define urep(i,k,n)    for(int i=k;i>=n;i--)
   #define hs            ios_base::sync_with_stdio(false);cin.tie(NULL);
   template <class C> void setmin(C &a,C b){if(a>b)a=b;}
   template <class C> void setmax(C &a,C b){if(a<b)a=b;}
